3b18b82841d7031ef9f08b16106fbf704d792506
MuhsanJaved <muhsantech048@outlook.com>
2022-01-18 16:06:21 +0500

    complete

--- app/src/main/java/com/muhsantech/createtoast/MainActivity.java
+++ app/src/main/java/com/muhsantech/createtoast/MainActivity.java
@@ -1,15 +1,11 @@
 package com.muhsantech.createtoast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.muhsantech.createtoast.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,23 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         binding.btnToast.setOnClickListener(view -> {
-            // BuildToast Toast.makeText(getApplicationContext(), "This is My First Toast", Toast.LENGTH_LONG).show();
+            // This is Android Build Toast
+            // Toast.makeText(getApplicationContext(), "This is My First Toast", Toast.LENGTH_LONG).show();
 
             // Custom Toast
-            Toast toast = new Toast(this);
-            View view1 = getLayoutInflater().inflate(R.layout.custom_toast_layout, (ViewGroup) findViewById(R.id.viewContainer));
-            toast.setView(view1);
-            //toast.setText("");
-
-            TextView txtMsg = view1.findViewById(R.id.txtMsg);
-            txtMsg.setText("Message sent Successfully");
-            toast.setDuration(Toast.LENGTH_LONG);
-
-            toast.setGravity(Gravity.CENTER, 0,0);
-
-            toast.show();
+            Toast toast = new Toast(this); // Creating TOAST
+            // ADD your TOAST Layout or layout find ID
+            View viewT = getLayoutInflater().inflate(R.layout.custom_toast_layout, findViewById(R.id.viewContainer));
+            toast.setView(viewT); // set View
+            //toast.setText(""); // Not Used this Method
+            TextView txtMsg = viewT.findViewById(R.id.txtMsg); // Find ID
+            txtMsg.setText(R.string.showMessage); // Set Your Message
+            toast.setDuration(Toast.LENGTH_LONG); // Set Toast Duration
+            toast.setGravity(Gravity.CENTER, 0,0); // Set Toast Gravity
+            toast.show(); // this very Import method you cannot write this method, Not show TOAST.......
         });
 
 
